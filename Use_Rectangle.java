7a2083213d8c1df82f49e49f6dfbc4dbea2589cd
shahma2001 <24mcam02@meaec.edu.in>
2025-02-18 10:40:25 +0530

    Add files via upload

---
+++ Use_Rectangle.java
@@ -0,0 +1,13 @@
+ class Use_Rectangle {
+    public static void main(String args[]) {
+        Rectangle rect = new Rectangle();
+        rect.length = 15.854;
+        rect.width = 22.65;
+        System.out.println("Length = " + rect.length);
+        System.out.println("Width = " + rect.width);
+        rect.Area();
+        rect.Perimeter();
+    }
+}
+
+
